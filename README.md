14217924ba6a0f667a7d134bed428b74e5ca554e
Christopher Santiago Ducey <csantiducey@Santi-Duceys-MacBook-Air.local>
2026-06-03 15:45:29 -0600

    Added pending files

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
-# RL-Algorithms-Benchmarking
-PettingZoo + Stable-Baselines 3 testing and comparison environment for RL algorithms.
+# Bench that RLearner using Stable-Baselines3 🤖
+
+Coding Template for a RL Pipeline that tests multiple learning algorithms (off-policy & on-policy) in a defined gymansium or custom environment. 
+
+⭐ Ideal for beginners in the area of Reinforcement Learning who are looking to learn by developing and testing different available learning algorithms, without having the need to possess an extensive mathematical or expertise background.
+
+## 📋 Table of Contents
+
+-  [About the Project](#-about-the-project)
+    -[Folder Structure](#folder-structure)
+-  [Prerequisites](#prerequisites)
+    -  [Installation](#installation)
+-  [Documentation](#documentation)
+-  [Licensing & Contact](#licensing--contact)
+
+
+## About the Project 
+
+The following software module provides the neccesary pipeline built over open-source RL frameworks to tune, test and compare the behavior of multiple RL learning algorithms and their learning performance over several custom environments.
+
+The pipeline is built on top of the following integration:
+
+**Gymnasium** _ _(environment library)_ _: Provides the simulated environments for agent training. Supports default small computationally-demanding environments, as well user-defined ones. 
+> Full documentation can be found in (#documentation) section.
+
+**Stable Baselines3** _ _(algorithm library)_ _: Provides the abstraction for the most used RL algorithms including but not limited to A2C, DQN, PPO, DDPG, SAC, TD3, etc. 
+
+### 📖 Folder Structure
+```text
+├── README.md
+├── eval_logs_best_a2c
+│   ├── best_model.zip
+│   └── evaluations.npz
+├── eval_logs_best_dqn
+│   ├── best_model.zip
+│   └── evaluations.npz
+├── fn_testing.py
+├── models
+│   └── LunarLander_best.zip
+├── parameters.py
+├── requirements.txt
+├── testing.py
+└── utils.py
+```
+
++ **eval_logs_best_a2c** -> Stores the best trained agent, its callback performance as well as the learning curve using the on-policy A2C algorithm.
+
++ **eval_logs_best_dqn** -> Stores the best trained agent, its callback performance as well as the learning curve using the off-policy DQN algorithm.
+
++ **fn_testing.py** -> Unittest module for testing proper function behavior.
+
++ **parameters.py** -> Hash Tables containing the optimized parameters for agent training.
+
++ **requirements.txt** -> Needed depencies and packages for proper project run.
+
++ **testing.py** -> Main file containing the project pipeline (Instantiation, Training, Evaluation, Rendering).
+
++ **utils.py** -> Contains additional helper functions for visualization.
+
+## Prerequisites
+
+It is recommended to have some general basic knowledge of what is Reinforcement Learning, the architectures behind it, and a high-overview of the mathematical foundations that enable each learning algorithm. Object-Oriented Programming is a must. 
+
+### ✅ Installation
+
+Run the following commands for proper environment setup and package installation. 
+
+In zsh terminal (macOS/Linux):
+
+1. Defined desired workspace
+```zsh
+cd path/to/your/folder
+```
+2. Generate folder to duplicate GitHub repository
+```zsh
+mkdir RL_Benchmarking
+```
+3. Clone remote repository locally
+```zsh
+git clone https://github.com/YOUR-USERNAME/YOUR-REPOSITORY
+```
+4. Create virtual environment
+```zsh
+python3 -m venv <venv_name>                               
+```
+5. Activate virtual environment
+```zsh
+source <venv_name>/bin/activate                            
+```
+6. Install required dependencies
+```zsh
+pip install -r requirements.txt                           
+```
+
+## 🧠 Documentation
+
+Here are the full documentations for both frameworks used, as well as the papers describing the state-of-the-art learning algorithms.
+
+    - Stable Baselines3: https://stable-baselines3.readthedocs.io/en/master/
+    - Gymnasium: https://gymnasium.farama.org/index.html
+
+    - A2C: [Asynchronous Methods for Deep Reinforcement Learning](https://arxiv.org/abs/1602.01783).
+
+    - DDPG: [Deterministic Policy Gradient](https://proceedings.mlr.press/v32/silver14.pdf)
+
+    - DQN: [Playing Atari with Deep Reinforcement Learning](https://arxiv.org/abs/1312.5602)
+
+    - PPO: [Proximal Policy Optimization Algorithms](https://stable-baselines3.readthedocs.io/en/master/modules/ppo.html)
+
+
+## ✉️ Licensing & Contact
+
+Santiago Ducey - [LinkedIn Profile](https://www.linkedin.com/in/csantiducey/) / santiagoduceychavez@gmail.com
+
+I've you've reached this far, never let your dreams go away.
+
+
+
--- testing.py
+++ testing.py
@@ -2,13 +2,18 @@
 
 import os
 import gymnasium as gym
-from stable_baselines3 import A2C, DQN
+import numpy as np
+import matplotlib.pyplot as plt
+
+from stable_baselines3 import A2C, DQN, TD3, SAC, PPO
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.callbacks import EvalCallback
-from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.env_util import make_vec_env, VecEnv
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 
 from utils import plot_learning_curve
+from parameters import A2C_optimized as a2c
+from parameters import DQN_optimized as dqn
 
 # Environments Documentations
 # (Discrete) -> "LunarLander-v3" : https://gymnasium.farama.org/environments/box2d/lunar_lander/
@@ -19,85 +24,84 @@ env_id = "LunarLander-v3"
 n_training_envs = 8
 n_eval_envs = 5
 
-# Optimized Model Parameters
-n_timesteps = int(4e5)
-learning_rate = 0.00083
-gamma = 0.995
-ent_coef = 0.0005
-steps = 32
-buffer_size = int(2e5)
+# Optimized Algorithm Parameters found in parameters.py
 
-# Create log dir to save evaluation results
-eval_log_dir_a2c = "./eval_logs_best_a2c/"
-eval_log_dir_dqn = "./eval_logs_best_dqn/"
-os.makedirs(eval_log_dir_a2c, exist_ok=True)
-os.makedirs(eval_log_dir_dqn, exist_ok=True)
 
-def main_process() -> None:
+def main_process(seed: int = 0) -> None:
     # Generate vectorized training environments (On-Policy and Off-Policy must not share)
-    train_env_a2c = make_vec_env(env_id, n_envs=n_training_envs, seed=0, vec_env_cls=SubprocVecEnv)
+    train_env_a2c = make_vec_env(env_id, n_envs=n_training_envs, seed=seed, vec_env_cls=SubprocVecEnv)
 
-    train_env_dqn = make_vec_env(env_id, n_envs=1, seed=0, vec_env_cls=DummyVecEnv)
+    train_env_dqn = make_vec_env(env_id, n_envs=1, seed=seed, vec_env_cls=DummyVecEnv)
 
-    # Shared Parallelized Evaluation Environments
-    eval_env = make_vec_env(env_id, n_envs=n_eval_envs, seed=0, vec_env_cls=DummyVecEnv)
+    # Instantiate distinct Parallelized Evaluation Environments
+    eval_env_a2c = make_vec_env(env_id, n_envs=n_eval_envs, seed=seed, vec_env_cls=DummyVecEnv)
+    eval_env_dqn = make_vec_env(env_id, n_envs=n_eval_envs, seed=seed, vec_env_cls=DummyVecEnv)
 
 
     # Create callback that evaluates agent
-    a2c_callback = EvalCallback(eval_env, best_model_save_path=eval_log_dir_a2c,
-                                log_path=eval_log_dir_a2c, eval_freq=max(int(1e5) // n_training_envs, 1), 
-                                deterministic=True, render=False
+    a2c_callback = EvalCallback(eval_env_a2c, best_model_save_path=eval_log_dir_a2c,
+                                log_path=eval_log_dir_a2c, eval_freq=max(a2c["n_timesteps"] // n_training_envs, 1), 
+                                deterministic=True, render=False, verbose=0
                                 )
 
-    dqn_callback = EvalCallback(eval_env, best_model_save_path=eval_log_dir_dqn,
-                                log_path=eval_log_dir_dqn, eval_freq=max(25000 // n_training_envs, 1),
-                                deterministic=True, render=False
+    dqn_callback = EvalCallback(eval_env_dqn, best_model_save_path=eval_log_dir_dqn,
+                                log_path=eval_log_dir_dqn, eval_freq=200000,
+                                deterministic=True, render=False, verbose=0
                                 )
 
     # Instantiate and generate Models with Optimized Parameters
-    model_a2c = A2C("MlpPolicy", 
-                    env=train_env_a2c, 
-                    learning_rate=learning_rate,         # O.00083
-                    n_steps=steps,                       # 32 -> Increment batch_size for smoother updating
-                    gamma=gamma,                         # 0.995 -> Highly Prioritize Future Reward
-                    ent_coef=ent_coef,                   # 0.0001
-                    device="cpu",                        # A2C optimized for CPU processing
+    model_a2c = A2C(policy  =   a2c["policy"], 
+                    env     =   train_env_a2c, 
+                    learning_rate = a2c["learning_rate"],         
+                    n_steps =   a2c["n_steps"],                                
+                    gamma   =   a2c["gamma"],                                
+                    ent_coef = a2c["ent_coef"], 
+                    vf_coef =   a2c["vf_coef"],               
+                    device  =   "cpu",                               # A2C optimized for CPU processing
+                    normalize_advantage = True,
+                    use_rms_prop    =   False,
+                    policy_kwargs   =   a2c["policy_kwargs"],
                     seed=0,                        
                     verbose=0
-                    )
+                )
 
-    model_dqn = DQN("MlpPolicy",
-                    env=train_env_dqn,
-                    learning_rate=learning_rate,
-                    buffer_size=buffer_size,            # 200,000 -> Bigger the better, limited only by computer RAM
-                    batch_size=256,                     # Experiences samples before gradient update
-                    gamma=gamma,
-                    train_freq=64,
+    model_dqn = DQN(policy  = dqn["policy"],
+                    env     = train_env_dqn,
+                    buffer_size     =   dqn["buffer_size"],           
+                    batch_size      =   dqn["batch_size"],                    
+                    gamma   =   dqn["gamma"],
+                    learning_starts =   dqn["learning_starts"],
+                    train_freq      =   dqn["train_freq"],
+                    gradient_steps  =   dqn["gradient_steps"],
+                    target_update_interval  =   dqn["target_update_interval"],
+                    exploration_fraction    =   dqn["exploration_fraction"],
+                    exploration_final_eps   =   dqn["exploration_final_eps"],
+                    policy_kwargs   =   dqn["policy_kwargs"],
                     device="auto",
                     seed=0,
                     verbose=0
-                    )
+                )
 
     # Train Models
-    print("------ STARTING TO TRAIN MODELS ------\n")
+    print("\n------ STARTING TO TRAIN MODELS ------\n")
 
     print("Training A2C...")
-    model_a2c.learn(total_timesteps=n_timesteps, callback=a2c_callback, progress_bar=True) # 400,000 steps / 8 environments = 50,000 steps x env
+    model_a2c.learn(total_timesteps=a2c["n_timesteps"], callback=a2c_callback, progress_bar=True) # 5,000,000 steps / 8 environments = 625,000 steps
     print("A2C Model trained ✅")
     
     print("\n Training DQN Model...")
-    model_dqn.learn(total_timesteps=int(1e5), callback=dqn_callback, progress_bar=True)
+    model_dqn.learn(total_timesteps=dqn["n_timesteps"], callback=dqn_callback, progress_bar=True)
     print("DQN Model trained ✅")
 
     print("\n📈 Evaluating Policy Performance...")
     # Compare and save Best Model
     a2c_mean, a2c_std = evaluate_policy(model=model_a2c, 
-                                        env=eval_env,
+                                        env=eval_env_a2c,
                                         n_eval_episodes=10,
                                         deterministic=True
                                         )
     dqn_mean, dqn_std = evaluate_policy(model=model_dqn,
-                                        env=eval_env,
+                                        env=eval_env_dqn,
                                         n_eval_episodes=10,
                                         deterministic=True
                                         )
@@ -108,21 +112,19 @@ def main_process() -> None:
     print(f"\nDQN Model -> Mean Reward: {dqn_mean:.2f} +/- {dqn_std:.2f}")
 
     # Learning Curve Rendering
-    plot_learning_curve(f"{eval_log_dir_a2c}evaluations.npz")
-    plot_learning_curve(f"{eval_log_dir_dqn}evaluations.npz")
+    plot_learning_curve(eval_log_dir_a2c)
+    plot_learning_curve(eval_log_dir_dqn)
 
     # Render Best Model
-    path = "models/LunarLander_best"
+    best_path = "models/LunarLander_best"
 
     if a2c_mean > dqn_mean :
         best_model = model_a2c
     else : 
         best_model = model_dqn
 
-    best_model.save(path)
+    best_model.save(best_path)
         
-    vec_env = best_model.get_env()
-
     print('\n🏆 Rendering Best Model Behavior...')
 
     vec_env = best_model.get_env()
@@ -135,21 +137,22 @@ def main_process() -> None:
         if dones.any():
             break
 
+    print("\n💯 Simulation finished, clossing all windows...")
+
     vec_env.close()
 
     return None
     
 
 if __name__ == '__main__' :
+
+    # Create log dirs to save performance metrics
+    eval_log_dir_a2c = "./eval_logs_best_a2c/"
+    eval_log_dir_dqn = "./eval_logs_best_dqn/"
+    os.makedirs(eval_log_dir_a2c, exist_ok=True)
+    os.makedirs(eval_log_dir_dqn, exist_ok=True)
+
     main_process()
-
-
-
-
-
-
-
-
 
 
 
